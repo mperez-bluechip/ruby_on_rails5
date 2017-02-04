c1c9806f3b788b881633efd11888e96eccdce1b9
Manny Perez <threedyou@gmail.com>
2017-02-04 13:00:42 -0600

    Deleted and readded project simple_cms to fix database and tables issues

--- Sites/simple_cms/config/routes.rb
+++ Sites/simple_cms/config/routes.rb
@@ -1,14 +1,3 @@
 Rails.application.routes.draw do
-
-  root 'demo#index'
-
-  get 'demo/index'
-  get 'demo/hello'
-  get 'demo/other_hello'
-  get 'demo/lynda'
-
-#default route / may go away in future verions of rails
-# get ':controller(/:action(/:id))'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- Sites/simple_cms/db/migrate/20170203134829_do_nothing_yet.rb
+++
@@ -1,7 +0,0 @@
-class DoNothingYet < ActiveRecord::Migration[5.0]
-  def up
-  end
-
-  def  down
-  end
-end
