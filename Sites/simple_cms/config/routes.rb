722ba91b6152473340d024b9a5d9f38c0c6e31f0
Manny Perez <threedyou@gmail.com>
2017-02-05 13:40:06 -0600

    Added deleted and mods to files fixing migration issues myslq2 gem install and phpMyAdmin table population

--- Sites/simple_cms/config/routes.rb
+++ Sites/simple_cms/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'demo#index'
+
+  get 'demo/index'
+  get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/lynda'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
